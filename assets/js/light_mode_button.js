965e8cf1d7904b1bea9eb8009eeb90083250fa6e
Romain Tribes <tribes.romain@gmail.com>
2020-03-15 19:33:11 +0100

    Save user preference for HD mode.

--- assets/js/light_mode_button.js
+++ assets/js/light_mode_button.js
@@ -5,25 +5,33 @@ export default class extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { hd: true };
+    const storedHd = localStorage.getItem('hd');
+    const hd = storedHd == null ? true : storedHd == 'yes';
+    this.state = { hd: hd };
     this.toggle = this.toggle.bind(this);
+  }
+
+  componentDidMount() {
+    if (this.state.hd == false) document.body.classList.add('light');
   }
 
 
   render() {
-    return <img src="/images/icons/icon_hd.png" id="toggle-hd-button"
-      onClick={this.toggle} className={this.state.hd ? "active" : "muted"} />;
+    return <img src='/images/icons/icon_hd.png' id='toggle-hd-button'
+      onClick={this.toggle} className={this.state.hd ? 'active' : 'muted'} />;
   }
 
 
   toggle() {
     if (this.state.hd) {
       this.setState({hd: false});
-      document.body.classList.add("light");
+      document.body.classList.add('light');
+      localStorage.setItem('hd', 'no');
     }
     else {
-       this.setState({hd: true });
-      document.body.classList.remove("light");
+      this.setState({hd: true });
+      document.body.classList.remove('light');
+      localStorage.setItem('hd', 'yes');
     }
   }
 }
