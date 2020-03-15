01ffdb90a8d6154913b5d8b412ff4d999b5adc21
Romain Tribes <tribes.romain@gmail.com>
2020-03-15 19:35:54 +0100

    Save user preference for background music.

--- assets/js/background_music.js
+++ assets/js/background_music.js
@@ -5,9 +5,12 @@ export default class extends React.Component {
   constructor(props) {
     super(props);
 
+    const storedMusic = localStorage.getItem('music');
+    const music = storedMusic == null ? true : storedMusic == 'yes';
+
     const audio = new Audio('/audio/spiky-whimsical-fantasy-the-docks.mp3');
     audio.loop = true;
-    audio.autoplay = true;
+    audio.autoplay = music;
 
     this.state = { audio: audio, muted: true };
     this.toggle = this.toggle.bind(this);
@@ -16,20 +19,27 @@ export default class extends React.Component {
 
   componentDidMount() {
     this.state.audio.addEventListener('play', function() {
-      this.setState({ muted: false })
+      this.setState({ muted: false });
     }.bind(this));
   }
 
 
   render() {
     return <img id='toggle-music-button' src='/images/icons/icon_sound.png'
-      onClick={this.toggle} className={this.state.audio.paused ? "muted" : "active"} />;
+      onClick={this.toggle} className={this.state.audio.paused ? 'muted' : 'active'} />;
   }
 
 
   toggle() {
-    if (this.state.audio.paused) this.state.audio.play();
-    else this.state.audio.pause();
-    this.setState({muted: !this.state.muted});
+    if (this.state.audio.paused) {
+      this.state.audio.play();
+      this.setState({ muted: false });
+      localStorage.setItem('music', 'yes');
+    }
+    else {
+      this.state.audio.pause();
+      this.setState({ muted: true });
+      localStorage.setItem('music', 'no');
+    }
   }
 }
