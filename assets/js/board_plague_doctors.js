c7a4c1094be0e21dd374fb893592ca49c9136599
Romain Tribes <tribes.romain@gmail.com>
2020-03-17 15:29:20 +0100

    Fix plague doctors apparition.

--- assets/js/board_plague_doctors.js
+++ assets/js/board_plague_doctors.js
@@ -11,7 +11,7 @@ export default class PlagueDoctors extends React.Component {
 
 
   shouldComponentUpdate () {
-    return this.phase == 'disabling';
+    return this.props.phase == 'disabling';
   }
 
 
