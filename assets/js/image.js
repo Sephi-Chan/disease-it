b7d47d9ce8ab7c2b1ff64ccdf348810393dbbc19
Romain Tribes <tribes.romain@gmail.com>
2020-03-25 23:21:18 +0100

    Remove useless log.

--- assets/js/image.js
+++ assets/js/image.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 
 export default function Image({src, ...others}) {
-  console.log(others);
-
   return <picture>
     <source srcSet={`${src}.webp`} type='image/webp' />
     <img src={src} {...others} />
